339dc234be59211b8308c7690f7634170e2e186c
bernblend <bernblend@gmail.com>
2017-03-31 11:10:56 -0700

    Added testing_append.py

---
+++ testing_append.py
@@ -0,0 +1,12 @@
+# Testing Append
+# What value is printed after running this code?
+
+p =[1,2]
+q = [3,4]
+p.append(q)
+print len(p)
+
+
+# Anser: 3
+# This is because a list is being appended but
+# it counts as only one element.
