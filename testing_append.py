a6cdbec77055e33ec22f866643e238142e687dcd
bernblend <bernblend@gmail.com>
2017-03-31 11:21:03 -0700

    Added content to testing_append.py

--- testing_append.py
+++ testing_append.py
@@ -10,3 +10,16 @@ print len(p)
 # Anser: 3
 # This is because a list is being appended but
 # it counts as only one element.
+
+# What will happen in this case?
+
+q[1] = 5
+print p
+
+
+# It changes the value of 'p', because the
+# object that was created, the third element
+# of 'p', is actually a reference to the object
+# containing [3,4], which is the same list object
+# that 'q' refrences.
+# So what we change in 'q' is also changed in 'p'.
