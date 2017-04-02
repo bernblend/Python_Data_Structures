d23bae3a62ee9d093dc045467564c3a61ec6a67b
bernblend <bernblend@gmail.com>
2017-04-02 02:28:29 -0700

    Added tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,5 @@
+#
+
+current_position = {"top left": " ", "top center": " ", "top right": " ",
+            "center left": " ", "center": " ", "center right": " ",
+            "bottom left": " ", "bottom center": " ", "bottom right": " "}
