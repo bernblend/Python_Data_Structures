04dc7892dd24e39d744dc978b34ffb488abfc62a
bernblend <bernblend@gmail.com>
2017-04-01 23:27:14 -0700

    Added index_method.py

---
+++ index_method.py
@@ -0,0 +1,15 @@
+
+
+
+p = [0, 1, 2]
+print p.index(2)
+# OUTPUT: 2    because that is the index where '2' appears in
+#              the list.
+
+
+p = [0, 1, 2, 2, 2]
+print p.index(2)
+# OUTPUT: 2    because that is the index where '2' appears in
+#              the list FIRST.
+
+# NOTE: if '2' were not in the list it would return an error.
