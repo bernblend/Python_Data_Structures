7d524b18075bc6a20b65f726d0b0fb81227b03e7
bernblend <bernblend@gmail.com>
2017-04-01 23:33:09 -0700

    Added index_method.py

--- index_method.py
+++ index_method.py
@@ -13,3 +13,20 @@ print p.index(2)
 #              the list FIRST.
 
 # NOTE: if '2' were not in the list it would return an error.
+
+
+
+
+print "\nUSING 'in' with a different context "
+print "than in the 'for loop'.\n"
+
+
+print "\n Syntax:    <value> in <list>"
+
+print "English: is '3' in the list?\n"
+
+print "Python:  '3 in p'  "
+
+
+p = [0, 1, 2]
+print 3 in p
