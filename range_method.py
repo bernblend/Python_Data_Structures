96651a0fc612957ebb8aa3ef2dc391be249e282c
bernblend <bernblend@gmail.com>
2017-04-02 00:36:51 -0700

    Added example to range_method.py

--- range_method.py
+++ range_method.py
@@ -2,6 +2,15 @@
 # range() with return a list of the size of the parameter:
 # NOT including the parameter number due to index '0'.
 
+print "\nrange(10)\n"
+
 print range(10)
 
+print "\nrange(25)\n"
+
 print range(25)
+
+print "\nUse of str() to convert int to string.\n"
+
+for n in range (20):
+    print "bern! The number is " + str(n)
