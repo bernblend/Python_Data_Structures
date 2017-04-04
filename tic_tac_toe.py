661ecc5fff4fe3a90ff116abc17ec146f605dad2
bernblend <bernblend@gmail.com>
2017-04-03 23:01:38 -0700

    Added dictionary example to tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,3 +10,11 @@ current_player = "X"
 
 some_string = "Hello {a_value}, {some_value}".format(a_value = "x", some_value = "y")
 print some_string
+
+
+
+print "\nANOTHER WAY: Using a Dictionary\n"
+
+some_dictionary = {"a_value" : "x", "some_value" : "y"}
+some_string = "Hello {a_value}, {some_value}".format(**some_dictionary)
+print some_string
