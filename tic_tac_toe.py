3839a76b1963c7095bf82ed658a0eebfd2aec495
bernblend <bernblend@gmail.com>
2017-04-03 22:29:12 -0700

    Added current_player

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,3 +3,6 @@
 current_position = {"top left": " ", "top center": " ", "top right": " ",
             "center left": " ", "center": " ", "center right": " ",
             "bottom left": " ", "bottom center": " ", "bottom right": " "}
+
+
+current_player = "X"
