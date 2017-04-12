43543c20a7e9bfad7fba1f5c3639b929795f25bf
bernblend <bernblend@gmail.com>
2017-04-11 23:57:14 -0700

    Added nextDay() to myAgeInDays.py

--- myAgeInDays.py
+++ myAgeInDays.py
@@ -21,3 +21,29 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
     # Your code here.
     ##
     return days
+
+
+
+###
+### Define a simple nextDay procedure, that assumes
+### every month has 30 days.
+###
+### For example:
+###    nextDay(1999, 12, 30) => (2000, 1, 1)
+###    nextDay(2013, 1, 30) => (2013, 2, 1)
+###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
+###
+
+def nextDay(year, month, day):
+    """
+    Returns the year, month, day of the next day.
+    Simple version: assume every month has 30 days.
+    """
+    day += 1
+    if day > 30:
+        day = 1
+        month += 1
+    if month > 12:
+        month = 1
+        year += 1
+    return  year, month, day
